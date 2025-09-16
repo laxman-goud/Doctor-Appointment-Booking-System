7c9c4a6ade72d63b71a62764b2d2af11365b21b7
laxman-goud <codewithlaxman92@gmail.com>
2025-09-16 07:26:35 +0530

    feat(client): add related doctors component to appointment page

--- client/src/pages/Appointment.jsx
+++ client/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctors from '../components/RelatedDoctors'
 
 const Appointment = () => {
     const { docId } = useParams()
@@ -146,6 +147,8 @@ const Appointment = () => {
                         </div>
                         <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
                     </div>
+                    {/* Listing related doctors */}
+                    <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
                 </>
             )}
         </div>
