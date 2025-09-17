2e873c9f088ff81303b8a181d71a35338ed4fad3
laxman-goud <codewithlaxman92@gmail.com>
2025-09-17 15:34:01 +0530

    chore(server): add db connection method

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import express from "express"
 import cors from "cors"
 import "dotenv/config"
+import connectDB from "./config/mongodb.js"
 
 const app = express()
 const port = process.env.PORT || 4000
+connectDB();
 
 app.use(express.json())
-app.use(cors()) 
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("API Working   ")
