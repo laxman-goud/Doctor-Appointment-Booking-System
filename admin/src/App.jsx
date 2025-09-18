a2af4da3980a0969a4c71709198324a2b0e7a354
laxman-goud <codewithlaxman92@gmail.com>
2025-09-18 20:08:51 +0530

    feat(admin):  connect form to server adminLogin function

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { AdminContext } from './context/AdminContext.jsx'
+import { useContext } from 'react'
 
 const App = () => {
-  return (
+
+  const {aToken} = useContext(AdminContext);
+
+  return aToken ? (
     <div>
-      <Login />
+      <ToastContainer />
     </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
   )
 }
 
