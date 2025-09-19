487f820bfcbe19d6e8a737427a7a25ebc864ec8b
laxman-goud <codewithlaxman92@gmail.com>
2025-09-19 07:41:31 +0530

    feat(admin): add Sidebar component  to app

---
+++ admin/src/components/Sidebar.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { AdminContext } from '../context/AdminContext.jsx'
+import { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../assets/assets.js'
+
+const Sidebar = () => {
+
+    const { aToken } = useContext(AdminContext)
+
+    return (
+        <div className='min-h-screen bg-white border-r'>
+            {
+                aToken && <ul className='text=[#515151] mt-5'>
+                    <NavLink
+                        to="/admin-dashboard"
+                        className={({ isActive }) =>
+                            `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
+                            }`
+                        }
+                    >
+                        <img src={assets.home_icon} alt="home" />
+                        <p>Dashboard</p>
+                    </NavLink>
+
+                    <NavLink
+                        to="/all-appointments"
+                        className={({ isActive }) =>
+                            `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
+                            }`
+                        }
+                    >
+                        <img src={assets.appointment_icon} alt="appointment" />
+                        <p>Appointments</p>
+                    </NavLink>
+
+                    <NavLink
+                        to="/add-doctor"
+                        className={({ isActive }) =>
+                            `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
+                            }`
+                        }
+                    >
+                        <img src={assets.add_icon} alt="add" />
+                        <p>Add Doctor</p>
+                    </NavLink>
+
+                    <NavLink
+                        to="/doctor-list"
+                        className={({ isActive }) =>
+                            `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
+                            }`
+                        }
+                    >
+                        <img src={assets.people_icon} alt="people" />
+                        <p>Doctors List</p>
+                    </NavLink>
+                </ul>
+            }
+        </div>
+    )
+}
+
+export default Sidebar
