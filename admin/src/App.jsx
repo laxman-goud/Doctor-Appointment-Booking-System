e2295a854eb9920f4a39552b6beecd065dbf37af
laxman-goud <codewithlaxman92@gmail.com>
2025-09-19 07:40:59 +0530

    feat(admin): initialize and route admin pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,12 @@ import 'react-toastify/dist/ReactToastify.css'
 import { AdminContext } from './context/AdminContext.jsx'
 import { useContext } from 'react'
 import Navbar from './components/Navbar.jsx'
+import Sidebar from './components/Sidebar.jsx'
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Admin/Dashboard.jsx'
+import AllAppointments from './pages/Admin/AllAppointments.jsx'
+import AddDoctor from './pages/Admin/AddDoctor.jsx'
+import DoctorsList from './pages/Admin/DoctorsList.jsx'
 
 const App = () => {
 
@@ -14,6 +20,16 @@ const App = () => {
     <div className='bg-[#F8F9FD]'>
       <ToastContainer />
       <Navbar />
+      <div className='flex items-start'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
