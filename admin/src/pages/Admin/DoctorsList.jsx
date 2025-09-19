a68b0842d82f91a38a0b9efd41bb58c7d46495b5
laxman-goud <codewithlaxman92@gmail.com>
2025-09-19 19:36:43 +0530

    feat(admin): add changeAvailability function to input checkbox

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -3,7 +3,7 @@ import { AdminContext } from '../../context/AdminContext.jsx'
 
 const DoctorsList = () => {
 
-    const { doctors, aToken, getAllDoctors } = useContext(AdminContext);
+    const { doctors, aToken, getAllDoctors, changeAvailability } = useContext(AdminContext);
 
     useEffect(() => {
         if (aToken) {
@@ -21,9 +21,9 @@ const DoctorsList = () => {
                             <img className='bg-indigo-50 group-hover:bg-primary transition-all duration-500' src={item.image} alt="doctor-image" />
                             <div className='p-4'>
                                 <p className='text-neutral-800 text-lg font-medium'>{item.name}</p>
-                                <p className='text-zinc-600 text-sm'>{item.speciality}</p>
+                                <p className='text-zinc-600 text-sm'>{item.speciality}</p>  
                                 <div className='mt-2 flex items-center gap-1 text-sm'>
-                                    <input className='accent-blue-600' type="checkbox" checked={item.available} />
+                                    <input onChange={() => changeAvailability(item._id)} className='accent-blue-600' type="checkbox" checked={item.available} />
                                     <p>Available</p>
                                 </div>
                             </div>
