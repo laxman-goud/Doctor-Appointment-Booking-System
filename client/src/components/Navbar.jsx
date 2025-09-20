89da9af6887b872958808e18f6cc99e5542057a6
laxman-goud <codewithlaxman92@gmail.com>
2025-09-20 09:58:38 +0530

    feat(client): fetch and display profile picture from database

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { AppContext } from '../context/AppContext.jsx';
 const Navbar = () => {
 
     const navigate = useNavigate();
-    const { token, setToken } = useContext(AppContext);
+    const { token, setToken, userData } = useContext(AppContext);
 
     const [showMenu, setShowMenu] = useState(false);
 
@@ -37,10 +37,10 @@ const Navbar = () => {
                 </NavLink>
             </ul>
             <div className='flex items-center gap-4'>
-                {token ?
+                {token && userData ?
                     (
                         <div className='flex items-center gap-2 cursor-pointer group relative'>
-                            <img className='w-8 rounded-full' src={assets.profile_pic} alt="profile_pic" />
+                            <img className='w-8 rounded-full' src={userData.image } alt="profile_pic" />
                             <img className='w-2.5' src={assets.dropdown_icon} alt="dropdown" />
                             <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                                 <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
