e842e1a2faed2c73d05a8455e3070b32265e7422
laxman-goud <codewithlaxman92@gmail.com>
2025-09-21 16:33:46 +0530

    feat(admin): add doctor routes to app

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,23 +11,34 @@ import Dashboard from './pages/Admin/Dashboard.jsx'
 import AllAppointments from './pages/Admin/AllAppointments.jsx'
 import AddDoctor from './pages/Admin/AddDoctor.jsx'
 import DoctorsList from './pages/Admin/DoctorsList.jsx'
+import { DoctorContext } from './context/DoctorContext.jsx'
+import DoctorDashboard from './pages/Doctor/DoctorDashboard.jsx'
+import DoctorProfile from './pages/Doctor/DoctorProfile.jsx'
+import DoctorAppoinments from './pages/Doctor/DoctorAppoinments.jsx'
 
 const App = () => {
 
   const {aToken} = useContext(AdminContext);
+  const {dToken} = useContext(DoctorContext);
 
-  return aToken ? (
+  return aToken || dToken ? (
     <div className='bg-[#F8F9FD]'>
       <ToastContainer />
       <Navbar />
       <div className='flex items-start'>
         <Sidebar />
         <Routes>
+          {/* Admin routes */}
           <Route path='/' element={<></>} />
           <Route path='/admin-dashboard' element={<Dashboard />} />
           <Route path='/all-appointments' element={<AllAppointments />} />
           <Route path='/add-doctor' element={<AddDoctor />} />
           <Route path='/doctor-list' element={<DoctorsList />} />
+
+          {/* Doctor routes */}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppoinments />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
         </Routes>
       </div>
     </div>
---
+++ admin/src/pages/Doctor/DoctorAppoinments.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DoctorAppoinments = () => {
+    return (
+        <div>DoctorAppoinments</div>
+    )
+}
+
+export default DoctorAppoinments
