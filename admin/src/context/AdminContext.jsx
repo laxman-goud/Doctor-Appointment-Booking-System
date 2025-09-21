8b56c9b0e3aded465c6587033a6416730327b3bd
laxman-goud <codewithlaxman92@gmail.com>
2025-09-21 15:59:54 +0530

    feat(admin): add Dashboard page UI and functionality

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -10,6 +10,7 @@ const AdminContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [doctors, setDoctors] = useState([]);
     const [appointments, setAppointments] = useState([]);
+    const [dashData, setDashData] = useState(false);
 
     const getAllDoctors = async () => {
         try {
@@ -66,6 +67,19 @@ const AdminContextProvider = (props) => {
             toast.error(error.message);
         }
     }
+    const getDashData = async () => {
+        try {
+            const { data } = await axios.get(`${backendUrl}/api/admin/dashboard`, { headers: { token: aToken } });
+            if (data.success) {
+                setDashData(data.dashData);
+            }
+            else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
 
     const value = {
         aToken, setAToken,
@@ -74,7 +88,8 @@ const AdminContextProvider = (props) => {
         changeAvailability,
         appointments, setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData, getDashData
     }
 
     return (
