4d40772e0f598c113c2209abd4ecadc2a9724358
laxman-goud <codewithlaxman92@gmail.com>
2025-09-21 22:04:05 +0530

    fix(client): fix doctor available status

--- client/src/pages/Doctors.jsx
+++ client/src/pages/Doctors.jsx
@@ -11,30 +11,30 @@ const Doctors = () => {
     const navigate = useNavigate()
 
     const applyFilter = () => {
-        if(specialty){
+        if (specialty) {
             setFilterDoc(doctors.filter(doc => doc.speciality === specialty))
         }
-        else{
+        else {
             setFilterDoc(doctors)
         }
     }
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         applyFilter()
     }, [doctors, specialty])
 
     return (
         <div>
             <p className='text-gray-600'>Browse through the doctors specialist.</p>
-            <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`} onClick={()=>{setShowFilter(prev => !prev)}}>Filters</button>
+            <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`} onClick={() => { setShowFilter(prev => !prev) }}>Filters</button>
             <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
                 <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
-                    <p onClick={()=>{specialty === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "General physician" ? "bg-indigo-100 text-black" : ""}`}>General physician</p>
-                    <p onClick={()=>{specialty === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>Gynecologist</p>
-                    <p onClick={()=>{specialty === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}>Dermatologist</p>
-                    <p onClick={()=>{specialty === 'Pediatricians' ? navigate('/doctors') : navigate('/doctors/Pediatricians')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "Pediatricians" ? "bg-indigo-100 text-black" : ""}`}>Pediatricians</p>
-                    <p onClick={()=>{specialty === 'Neurologist' ? navigate('/doctors') : navigate('/doctors/Neurologist')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "Neurologist" ? "bg-indigo-100 text-black" : ""}`}>Neurologist</p>
-                    <p onClick={()=>{specialty === 'Gastroenterologist' ? navigate('/doctors') : navigate('/doctors/Gastroenterologist')}} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "Gastroenterologist" ? "bg-indigo-100 text-black" : ""}`}>Gastroenterologist</p>
+                    <p onClick={() => { specialty === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician') }} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "General physician" ? "bg-indigo-100 text-black" : ""}`}>General physician</p>
+                    <p onClick={() => { specialty === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist') }} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>Gynecologist</p>
+                    <p onClick={() => { specialty === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist') }} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}>Dermatologist</p>
+                    <p onClick={() => { specialty === 'Pediatricians' ? navigate('/doctors') : navigate('/doctors/Pediatricians') }} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "Pediatricians" ? "bg-indigo-100 text-black" : ""}`}>Pediatricians</p>
+                    <p onClick={() => { specialty === 'Neurologist' ? navigate('/doctors') : navigate('/doctors/Neurologist') }} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "Neurologist" ? "bg-indigo-100 text-black" : ""}`}>Neurologist</p>
+                    <p onClick={() => { specialty === 'Gastroenterologist' ? navigate('/doctors') : navigate('/doctors/Gastroenterologist') }} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${specialty === "Gastroenterologist" ? "bg-indigo-100 text-black" : ""}`}>Gastroenterologist</p>
                 </div>
                 <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
                     {
@@ -42,8 +42,8 @@ const Doctors = () => {
                             <div onClick={() => { navigate(`/appointment/${item._id}`) }} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                                 <img className='bg-blue-50' src={item.image} alt="doctor" />
                                 <div className='p-4'>
-                                    <div className='flex items-center gap-2 text-sm text-center text-green-500'>
-                                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                                    <div className={`flex items-center gap-2 text-sm text-center ${item.available ? 'text-green-500' : 'text-gray-500'}`}>
+                                        <p className={`w-2 h-2 ${item.available ? 'bg-green-500' : 'bg-gray-500'} rounded-full`}></p><p>{item.available ? 'Available' : 'Not Available'}</p>
                                     </div>
                                     <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
                                     <p className='text-gray-600 text-sm'>{item.speciality}</p>
--- client/src/pages/MyAppointments.jsx
+++ client/src/pages/MyAppointments.jsx
@@ -110,10 +110,11 @@ const MyAppointments = () => {
                         </div>
                         <div></div>
                         <div className='flex flex-col gap-2 justify-end'>
-                            {!item.cancelled && item.payment && <button className='text-sm text-stone-500 text-center bg-gray-200 sm:min-w-48 py-2 border rounded hover:bg-gray-400 hover:text-white transition-all duration-300'>Paid</button>}
-                            {!item.cancelled && !item.payment  && <button onClick={()=>{appointmentRazorpay(item._id)}} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
-                            {!item.cancelled && <button onClick={()=>{cancelAppointment(item._id)}} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>}
-                            {item.cancelled && <button className='text-sm text-red-600 text-center sm:min-w-48 py-2 border border-red-500 rounded hover:bg-red-500 hover:text-white transition-all duration-300'>Appointment cancelled</button>}
+                            {!item.cancelled && item.payment && !item.isCompleted && <button className='text-sm text-stone-500 text-center bg-gray-200 sm:min-w-48 py-2 border rounded hover:bg-gray-400 hover:text-white transition-all duration-300'>Paid</button>}
+                            {!item.cancelled && !item.payment && !item.isCompleted  && <button onClick={()=>{appointmentRazorpay(item._id)}} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
+                            {!item.cancelled && !item.isCompleted &&  <button onClick={()=>{cancelAppointment(item._id)}} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>}
+                            {item.cancelled && !item.isCompleted && <button className='text-sm text-red-600 text-center sm:min-w-48 py-2 border border-red-500 rounded hover:bg-red-500 hover:text-white transition-all duration-300'>Appointment cancelled</button>}
+                            {item.isCompleted && <button className='text-sm text-green-600 text-center sm:min-w-48 py-2 border border-green-500 rounded hover:bg-green-500 hover:text-white transition-all duration-300'>Completed</button>}
                         </div>
                     </div>
                 ))}
