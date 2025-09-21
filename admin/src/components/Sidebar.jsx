ec67b81bb55b74f71eb32c2fa093d5f6a5877543
laxman-goud <codewithlaxman92@gmail.com>
2025-09-21 16:34:19 +0530

    feat(admin): add sidebar for doctor

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -3,10 +3,12 @@ import { AdminContext } from '../context/AdminContext.jsx'
 import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets.js'
+import { DoctorContext } from '../context/DoctorContext.jsx'
 
 const Sidebar = () => {
 
-    const { aToken } = useContext(AdminContext)
+    const { aToken } = useContext(AdminContext);
+    const {dToken} = useContext(DoctorContext);
 
     return (
         <div className='min-h-screen bg-white border-r'>
@@ -54,6 +56,43 @@ const Sidebar = () => {
                     >
                         <img src={assets.people_icon} alt="people" />
                         <p>Doctors List</p>
+                    </NavLink>
+                </ul>
+            }
+
+            {
+                dToken && <ul className='text=[#515151] mt-5'>
+                    <NavLink
+                        to="/doctor-dashboard"
+                        className={({ isActive }) =>
+                            `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
+                            }`
+                        }
+                    >
+                        <img src={assets.home_icon} alt="home" />
+                        <p>Dashboard</p>
+                    </NavLink>
+
+                    <NavLink
+                        to="/doctor-appointments"
+                        className={({ isActive }) =>
+                            `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
+                            }`
+                        }
+                    >
+                        <img src={assets.appointment_icon} alt="appointment" />
+                        <p>Appointments</p>
+                    </NavLink>
+
+                    <NavLink
+                        to="/doctor-profile"
+                        className={({ isActive }) =>
+                            `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
+                            }`
+                        }
+                    >
+                        <img src={assets.people_icon} alt="people" />
+                        <p>Profile</p>
                     </NavLink>
                 </ul>
             }
