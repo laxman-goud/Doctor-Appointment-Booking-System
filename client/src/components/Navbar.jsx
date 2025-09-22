a74477a2b0f1bac2af3ed71dd1d8efdbc7b295a9
laxman-goud <codewithlaxman92@gmail.com>
2025-09-22 11:35:06 +0530

    style(client): fix user size image in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -40,8 +40,8 @@ const Navbar = () => {
                 {token && userData ?
                     (
                         <div className='flex items-center gap-2 cursor-pointer group relative'>
-                            <img className='w-8 rounded-full' src={userData.image } alt="profile_pic" />
-                            <img className='w-2.5' src={assets.dropdown_icon} alt="dropdown" />
+                            <img className='w-8 h-8 rounded-full' src={userData.image } alt="profile_pic" />
+                            <img className='w-3' src={assets.dropdown_icon} alt="dropdown" />
                             <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                                 <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                                     <p onClick={()=>{navigate('/my-profile')}} className='hover:text-black cursor-pointer'>My Profile</p>
