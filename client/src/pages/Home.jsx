95234912d84dde65130e15364499aab3c9cd9cb3
laxman-goud <codewithlaxman92@gmail.com>
2025-11-07 18:36:41 +0530

    💬 docs(client): add inline comments to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+// Importing necessary components used on the homepage
 import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
@@ -7,9 +8,16 @@ import Banner from '../components/Banner'
 const Home = () => {
     return (
         <div>
+            {/* Header Section */}
             <Header />
+
+            {/* Displays various medical specialties */}
             <SpecialityMenu />
+
+            {/* Section showcasing top-rated doctors */}
             <TopDoctors />
+
+            {/* Promotional or informational banner section */}
             <Banner />
         </div>
     )
